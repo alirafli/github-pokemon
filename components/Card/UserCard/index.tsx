88d436963612e6c167a75ae69a5301543c5a5112
alirafli <mrafli1212@gmail.com>
2022-12-09 22:50:53 +0700

    feat: user detail data

--- components/Card/UserCard/index.tsx
+++ components/Card/UserCard/index.tsx
@@ -2,6 +2,8 @@ import { ATagButton } from "../../Button";
 import Text from "../../Text";
 import { userDetail } from "../../../data";
 import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
+import basePoint from "../../../pages/api/basePoint";
 
 type Props = {
   login: string;
@@ -9,25 +11,50 @@ type Props = {
   html_url: string;
 };
 
+type userDetailData = {
+  name: string;
+  bio: string;
+  location: string;
+};
+
 export const UserCard = ({
   login = "username",
   avatar_url = "nickname",
   html_url = "your title",
 }: Props) => {
-  const userData = userDetail;
+  const [userDetail, setuserDetail] = useState<userDetailData>({
+    name: "your name",
+    bio: "this is my bio",
+    location: "world",
+  });
+
+  const getUserDetail = useCallback(async () => {
+    try {
+      const res = await basePoint.get(`users/${login}`);
+
+      console.log(res.data);
+      setuserDetail(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [login]);
+
+  useEffect(() => {
+    getUserDetail();
+  }, [getUserDetail]);
   return (
     <div className="my-5 border-gray-600 border-b py-2">
       <div className="flex">
-       <div>
-       <Image
-          loader={() => avatar_url}
-          src={avatar_url}
-          width={35}
-          height={35}
-          alt="profile picture"
-          className="rounded-full"
-        />
-       </div>
+        <div>
+          <Image
+            loader={() => avatar_url}
+            src={avatar_url}
+            width={35}
+            height={35}
+            alt="profile picture"
+            className="rounded-full"
+          />
+        </div>
         <div className="ml-2 pt-1">
           <ATagButton to={html_url} className="flex">
             <Text
@@ -35,7 +62,7 @@ export const UserCard = ({
               weight="semiBold"
               className="text-blue-400 mr-2 no-underline hover:underline"
             >
-              {userData.name}
+              {userDetail.name}
             </Text>
             <Text
               variant="p2"
@@ -46,10 +73,10 @@ export const UserCard = ({
             </Text>
           </ATagButton>
           <Text variant="p2" className="my-2">
-            {userData.bio}
+            {userDetail.bio}
           </Text>
           <Text variant="p3" className="text-white/40">
-            {userData.location}
+            {userDetail.location}
           </Text>
         </div>
       </div>
